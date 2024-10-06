ac7386dee3219fef9e05bb6b07a9e609541eec8d
BugRequiem <bugrequiem@gmail.com>
2024-10-06 15:36:05 +0800

    update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 from enum import Enum
 from utils.socket import SocketServer
 from utils.camera import GstreamerCamera
-from utils.model import DetectModel
+from utils.model.sample import ModelSample
 import json
 import atexit
 
@@ -17,37 +17,21 @@ class App:
 
     def __init__(self, config):
         self.config = config
-        self.server = None
-        self.init_socket()
-        self.camera = None
-        self.init_camera()
-        self.model = DetectModel()
-        self.camera = GstreamerCamera()
-        self.lock = threading.Lock()
-        self.detect_flag = False
-        self.status = State.STOP
-    
-    def init_socket(self):
-        host = self.config['socket']['host']
-        port = self.config['socket']['port']
-        self.server = SocketServer(host, port)
-    
-    def init_camera(self):
-        device = self.config['camera']['device']
-        width = self.config['camera']['width']
-        height = self.config['camera']['height']
-        framerate = self.config['camera']['framerate']
-        pformat = self.config['camera']['pformat']
-        self.camera = GstreamerCamera(device, width, height, framerate, pformat)
-
+        self.server = SocketServer(self.config['socket'])       # 初始化socket
+        self.camera = GstreamerCamera(self.config['camera'])    # 初始化相机
+        self.model = ModelSample(self.config['model'])          # 初始化模型
+        self.lock = threading.Lock()                            # 声明互斥锁
+        self.detect_flag = False                                # 声明检测线程标志
+        self.status = State.STOP                                # 声明检测线程运行状态
     
     def run(self):
         self.server.start()
         while True:
-            # 等待接收客户端的启动消息
+            # 阻塞接收客户端消息
             receive_data = self.server.receive_json()
             if receive_data == None:
                 continue
+            # 接收到客户端的启动消息
             if self.status == State.STOP:  # 如果检测线程未启动
                 if receive_data['action'] == 'start':
                     response_data = {"status" : 'success', "message" : 'start detecting'}
@@ -56,7 +40,7 @@ class App:
                     self.detect_flag = True
                     detect_thread.start()
                     self.status = State.START
-            # 等待接收客户端的停止消息
+            # 接收到客户端的停止消息
             if self.status == State.START:  # 如果检测线程启动
                 if receive_data['action'] == 'stop':
                     with self.lock:
@@ -66,6 +50,7 @@ class App:
                     self.server.send_json(response_data)
                     self.status = State.STOP
     
+    # 推理与结果发送线程函数
     def send_results(self):
         while True:
             with self.lock:
@@ -81,11 +66,11 @@ class App:
             ]}
             start_time = time.time()
             frame = self.camera.read()              # 从摄像头读取帧
-            result = self.model.inference(frame)    # 模型推理获得结果
+            # result = self.model(frame=frame)      # 模型推理获得结果
             self.server.send_json(json_data)        # socket发送结果
             finish_time = time.time()
             task_time = finish_time - start_time    # 单次任务所用时间
-            time.sleep(1 - task_time)  # 每秒发送一条消息
+            time.sleep(1 - task_time)               # 每秒发送一条消息
 
     def stop(self):
         self.server.stop()
@@ -97,7 +82,7 @@ if __name__ == '__main__':
     with open('config.json', 'r', encoding='utf-8') as file:
         config = json.load(file)
         print(f'App config:\n{json.dumps(config, indent=4)}')
-
+    # 创建并启动服务器
     app = App(config)
     atexit.register(app.stop())
     app.run()
