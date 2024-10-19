df883e1eb5887f4a592754f495d90b78ec84884f
BugRequiem <bugrequiem@gmail.com>
2024-10-19 20:46:29 +0800

    update models/export.py

--- models/export.py
+++ models/export.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 # Load a model
-model = YOLO("runs/detect/trafficLight/best_wsj.pt")  # 载入预训练模型
+model = YOLO("runs/detect/trafficLight/traffic_light.pt")  # 载入预训练模型
 # Export the model
 success = model.export(format="engine",imgsz=640, half=True)  # 导出静态ONNX模型，需设置batch参数注意opset为12/13
