dafab3ab259060d46f6a8b0a5f3af96ae341b540
BugRequiem <bugrequiem@gmail.com>
2024-11-19 18:16:42 +0800

    update app_multiprocessing.py

--- app_multiprocessing.py
+++ app_multiprocessing.py
@@ -11,39 +11,39 @@ import sys
 import logging
 import atexit
 import os
+import socket
+import queue
 
 
-def detect(lock, config, shape, freq, detect_msg_queue, shared_frame_array, socket_queue, error_queue):
+def detect(capture_event, detect_event, lock, config, shape, freq,
+           shared_frame_array, socket_queue, error_queue):
     """
     模型推理进程
     """
-    # 自动进入暂停状态，等待开始消息
-    message = 'suspend'
     logger = logging.getLogger('app_logger')
     try:
-        model = Model(config['model'])
+        logger.info('try to init model...')
+        with lock:
+            model = Model(config['model'])
     except Exception as e:
         logger.error(f'Model init error: {e}')
         error_queue.put(("detect", str(e)))
         sys.exit(-1)
     logger.info("detect init success, start detect processing.")
     while True:
-        if not detect_msg_queue.empty():
-            message = detect_msg_queue.get()
-        if message == 'suspend':
-            message = detect_msg_queue.get()
-        elif message == 'stop':
-            sys.exit(0)
-        elif message == 'resume':
-            with lock:
-                frame = np.frombuffer(shared_frame_array.get_obj(), dtype=np.uint8).reshape(shape)
-            start_time = time.time()
-            try:
-                result = model(frame=frame)
-            except Exception as e:
-                logger.error(f'Model inference error: {e}')
-                error_queue.put(("detect", str(e)))
-                sys.exit(-1)
+        capture_event.wait()    # 保证摄像头读取到了图像
+        detect_event.wait()     # 保证推理开启
+        with lock:
+            frame = np.frombuffer(shared_frame_array.get_obj(), dtype=np.uint8).reshape(shape)
+        start_time = time.time()
+        try:
+            result = model(frame=frame)
+            logger.debug(f'detect result: {result}')
+        except Exception as e:
+            logger.error(f'Model inference error: {e}')
+            error_queue.put(("detect", str(e)))
+            sys.exit(-1)
+        else:
             finish_time = time.time()
             task_time = finish_time - start_time    # 单次任务所用时间
             logger.debug(f"tast_time = {task_time}")
@@ -53,59 +53,123 @@ def detect(lock, config, shape, freq, detect_msg_queue, shared_frame_array, sock
                 time.sleep(delay_time)
 
 
-def capture(lock, config, shape, cap_msg_queue, shared_frame_array, error_queue):
+def capture(capture_event, lock, config, shape,
+            shared_frame_array, error_queue):
     """
     捕获图像进程
     """
-    # 自动进入暂停状态，等待开始消息
-    message = 'suspend'
     logger = logging.getLogger('app_logger')
     try:
-        camera = GstreamerCamera(config['camera'])
+        logger.info('try to init camera...')
+        with lock:
+            camera = GstreamerCamera(config['camera'])
     except Exception as e:
+        # 暂停检测进程
+        capture_event.clear()
         camera.close()
         logger.error(f'Camera init error: {e}')
         error_queue.put(("capture", str(e)))
         sys.exit(-1)
     logger.info("capture init success, start capture processing.")
     while True:
-        if not cap_msg_queue.empty():
-            message = cap_msg_queue.get()
-        if message == 'suspend':
-            message = cap_msg_queue.get()
-        elif message == 'stop':
-            sys.exit(0)
-        elif message == 'resume':
-            try:
-                frame = camera.read()
-                logger.debug('read a frame')
-            except Exception as e:
-                camera.close()
-                logger.error(f'Camera read error: {e}')
-                error_queue.put(("capture", str(e)))
-                sys.exit(-1)
+        try:
+            frame = camera.read()
+        except Exception as e:
+            # 暂停检测进程
+            capture_event.clear()
+            camera.close()
+            logger.error(f'Camera read error: {e}')
+            error_queue.put(("capture", str(e)))
+            sys.exit(-1)
+        else:
             with lock:
                 np.frombuffer(shared_frame_array.get_obj(), dtype=np.uint8).reshape(shape)[:] = frame
+            # 摄像头读取图像就绪
+            capture_event.set()
             if config['camera']['mode'] == 'video':
                 time.sleep(1 / config['camera']['framerate'])
 
 
-def send():
+def dosocket(config, socket_queue, error_queue, detect_event):
+    """
+    socket相关进程
+    """
+    logger = logging.getLogger('app_logger')
+    # 设置线程的socket_queue,和error_queue
+    threading_socket_queue = queue.Queue()
+    threading_error_queue = queue.Queue()
+    threading_cmd_queue = queue.Queue()
+    # 初始化socket服务器
+    server = SocketServer(config['socket'])
+    # 获取本地ip
+    local_ip = server.get_host()
+    send_threading = threading.Thread(target=send, daemon=True, args=(
+        logger, server, threading_socket_queue, threading_error_queue
+    ))
+    receive_threading = threading.Thread(target=receive, daemon=True, args=(
+        logger, server, threading_socket_queue, threading_error_queue, threading_cmd_queue
+    ))
+    broadcast_treading = threading.Thread(target=broadcast, daemon=True, args=(
+        logger, local_ip, config['socket']['broadcast_port'], threading_error_queue
+    ))
+    # 广播线程启动
+    broadcast_treading.start()
+    # 等待客户端连接
+    server.start()
+    # 开启主进程的子线程
+    send_threading.start()
+    receive_threading.start()
+
+    # 单独与其他进程通信
+    while True:
+        if not socket_queue.empty():
+            message_temp = socket_queue.get()
+            threading_socket_queue.put(message_temp)
+        if not threading_error_queue.empty():
+            error_temp = threading_error_queue.get()
+            error_queue.put(error_temp)
+        if not threading_cmd_queue.empty():
+            cmd_temp = threading_cmd_queue.get()
+            if cmd_temp == 'start':
+                detect_event.set()
+            elif cmd_temp == 'stop':
+                detect_event.clear()
+
+def broadcast(logger, message:str, broadcast_port, threading_error_queue):
+    """
+    通过socket广播本机ip地址
+    """
+    # 创建UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    broadcast_address = '<broadcast>'
+    logger.info(f'broadcast local ip: {message}, broadcast port: {broadcast_port}')
+    try:
+        while True:
+            sock.sendto(message.encode('utf-8'), (broadcast_address, broadcast_port))
+            time.sleep(1)
+    except Exception as e:
+        logger.error("broadcast error.")
+        threading_error_queue.put(("broadcast", str(e)))
+    finally:
+        sock.close()
+
+def send(logger, server, threading_socket_queue, threading_error_queue):
     """
     发送socket消息的子线程
     """
     while True:
-        message = socket_queue.get()
+        message = threading_socket_queue.get()
         if message is None:
             return
         try:
             server.send_json(message)
         except Exception as e:
             logger.error(f'Socket send message error: {e}')
-            error_queue.put(("send", str(e)))
+            threading_error_queue.put(("send", str(e)))
             return
 
-def receive():
+def receive(logger, server, threading_socket_queue, threading_error_queue, threading_cmd_queue):
     """
     接收socket消息的子线程
     """
@@ -113,39 +177,40 @@ def receive():
         try:
             receive_data = server.receive_json()
             if receive_data is None:
-                socket_queue.put(None)
+                threading_socket_queue.put(None)
                 raise RuntimeError('None data received')
         except Exception as e:
             logger.error(f'Socket receive message error: {e}')
-            error_queue.put(("receive", str(e)))
+            threading_error_queue.put(("receive", str(e)))
             return
-        if receive_data['action'] == 'start':
-            response_data = {"status" : 'success', "message" : 'start detecting'}
-            cap_msg_queue.put('resume')         # 继续相机捕获进程
-            detect_msg_queue.put('resume')      # 继续模型检测进程
-            socket_queue.put(response_data)     # 将成功消息添加到socket消息队列
-        elif receive_data['action'] == 'stop':
-            response_data = {"status" : 'success', "message" : 'stop detecting'}
-            cap_msg_queue.put('suspend')        # 暂停相机捕获进程
-            detect_msg_queue.put('suspend')     # 暂停模型检测进程
-            socket_queue.put(response_data)     # 将成功消息添加到socket消息队列
+        # logger.debug(f'receive_data is: {receive_data}')
+        else:
+            if receive_data['action'] == 'start':
+                response_data = {"status" : 'success', "message" : 'start detecting'}
+                # 开始检测进程
+                threading_cmd_queue.put('start')
+                threading_socket_queue.put(response_data)     # 将成功消息添加到socket消息队列
+            elif receive_data['action'] == 'stop':
+                response_data = {"status" : 'success', "message" : 'stop detecting'}
+                # 暂停检测进程
+                threading_cmd_queue.put('stop')
+                threading_socket_queue.put(response_data)     # 将成功消息添加到socket消息队列
+
 
 def app_cleanup():
     if detect_processing.is_alive():
         detect_processing.terminate()
     if capture_processing.is_alive():
         capture_processing.terminate()
+    if dosocket_processing.is_alive():
+        dosocket_processing.terminate()
     detect_processing.join()
     capture_processing.join()
+    dosocket_processing.join()
     logger.info(f"app will restart after {restart_time} seconds")
-    server.stop()
     listener.stop()
     log_queue.close()
     log_queue.join_thread()
-    detect_msg_queue.close()
-    detect_msg_queue.join_thread()
-    cap_msg_queue.close()
-    cap_msg_queue.join_thread()
     error_queue.close()
     error_queue.join_thread()
     socket_queue.close()
@@ -169,13 +234,14 @@ if __name__ == '__main__':
 
     # 定义进程间共享变量，存放图像数据
     shared_frame_array = multiprocessing.Array('B', size)
-    # 定义互斥锁，用于访问共享资源shared_frame_array
+    # 定义互斥锁
     lock = multiprocessing.Lock()
+    # 定义事件
+    detect_event = multiprocessing.Event()
+    capture_event = multiprocessing.Event()
     # 定义消息队列
-    detect_msg_queue = multiprocessing.Queue()
-    cap_msg_queue = multiprocessing.Queue()
-    error_queue = multiprocessing.Queue()
-    socket_queue = multiprocessing.Queue()
+    error_queue = multiprocessing.Queue(-1)
+    socket_queue = multiprocessing.Queue(-1)
     log_queue = multiprocessing.Queue(-1)
 
     # 初始化logger
@@ -195,71 +261,75 @@ if __name__ == '__main__':
     # 打印app配置信息
     logger.debug(f'App config:\n{json.dumps(config, indent=4)}')
 
-    # 初始化socket服务器
-    server = SocketServer(config['socket'])
-
     # 初始化子进程
     detect_processing = multiprocessing.Process(target=detect,
-                                                args=(lock, config, shape, freq,
-                                                      detect_msg_queue,
+                                                args=(capture_event, detect_event,
+                                                      lock, config, shape, freq,
                                                       shared_frame_array,
                                                       socket_queue, error_queue))
     capture_processing = multiprocessing.Process(target=capture,
-                                                args=(lock, config, shape, 
-                                                      cap_msg_queue,
+                                                args=(capture_event,
+                                                      lock, config, shape, 
                                                       shared_frame_array,
                                                       error_queue))
-    # 初始化子线程
-    send_threading = threading.Thread(target=send, daemon=True)
-    receive_threading = threading.Thread(target=receive, daemon=True)
+    dosocket_processing = multiprocessing.Process(target=dosocket,
+                                                args=(config, socket_queue, 
+                                                      error_queue, detect_event))
 
     # 设置为守护进程，在主进程退出后子进程自动退出
     detect_processing.daemon = True
     capture_processing.daemon = True
+    dosocket_processing.daemon = True
     # 开启进程
+    dosocket_processing.start()
+    time.sleep(0.5)
     detect_processing.start()
+    time.sleep(0.5)
     capture_processing.start()
 
-    # 等待客户端连接
-    server.start()
 
-    # 开启主进程的子线程
-    send_threading.start()
-    receive_threading.start()
-
-    # 同一进行异常处理
+    # 统一进行异常处理
     while True:
         error = error_queue.get()
-        if error[0] == 'send' or error[0] == 'receive':
+        if error[0] == 'send' or error[0] == 'receive' or error[0] == 'broadcast':
             sys.exit(-1)
-        if error[0] == 'capture':
+        elif error[0] == 'capture':
             logger.error('failed in capture processing, suspend detect processing.')
+            detect_event.clear()
+            capture_processing.join()
             # 将异常消息加入socket_queue中
             response_data = {"status" : 'error', "message" : 'capture processing error.'}
             socket_queue.put(response_data)
-            # 暂停检测进程
-            detect_msg_queue.put("suspend")
-            capture_processing.join()
             # 尝试重新启动摄像头进程
             capture_processing = multiprocessing.Process(target=capture,
-                                                         args=(lock, config, shape, 
-                                                               cap_msg_queue,
-                                                               shared_frame_array,
-                                                               error_queue))
-            logger.info('try to restart capture processing...')
+                                                        args=(capture_event,
+                                                              lock, config, shape,
+                                                              shared_frame_array,
+                                                              error_queue))
+            logger.info('try to restart capture processing after 10 seconds...')
+            time.sleep(10)
             capture_processing.start()
         elif error[0] == 'detect':
-            logger.error('failed in detect processing, suspend capture processing.')
+            logger.error('failed in detect processing.')
+            # 检测进程置为未启动
+            detect_event.clear()
+            # 等待子进程退出
+            detect_processing.join()
             # 将异常消息加入socket_queue中
             response_data = {"status" : 'error', "message" : 'detect processing error.'}
             socket_queue.put(response_data)
-            # 暂停摄像头进程
-            cap_msg_queue.put("suspend")
-            detect_processing.join()
+            # 尝试重新启动检测进程
             detect_processing = multiprocessing.Process(target=detect,
-                                                        args=(lock, config, shape, freq,
-                                                                detect_msg_queue,
-                                                                shared_frame_array,
-                                                                socket_queue, error_queue))
-            logger.info('try to restart detect processing...')
+                                                        args=(capture_event, 
+                                                              detect_event, 
+                                                              lock, config, shape, freq,
+                                                              shared_frame_array,
+                                                              socket_queue, error_queue))
+            logger.info('try to restart detect processing after 10 seconds...')
+            time.sleep(10)
             detect_processing.start()
+        else:
+            logger.error('unknown error!')
+            response_data = {"status" : 'error', "message" : 'unknown error.'}
+            socket_queue.put(response_data)
+            sys.exit(-1)
